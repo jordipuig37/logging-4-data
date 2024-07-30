e366d828eef4dad6dab99ac584d9b93eccd33380
Jordi Puig <jpuig@t2client.com>
2024-07-30 11:30:18 +0200

    changing documentation version as well

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ sys.path.insert(0, os.path.abspath(".."))
 project = 'log4data'
 copyright = '2024, Jordi Puig Rabat'
 author = 'Jordi Puig Rabat'
-release = '0.1.1'
+release = '0.1.2'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
