b71deaeb9fdf49cb5cd5ffde9fefa668f5562c95
Jordi Puig <jpuig@t2client.com>
2024-07-30 08:49:07 +0200

    Added extensions to docs conf

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,13 @@ release = '0.1.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.todo", "sphinx.ext.viewcode", "sphinx.ext.autodoc"]
+extensions = [
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.coverage",
+    "sphinx.ext.napoleon"
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
